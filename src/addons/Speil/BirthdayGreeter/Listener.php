3eab96697862903b6728eaeb91c050b689f689fb
Speil <jochenschaefer86@googlemail.com>
2025-06-25 15:41:44 +0200

    Update Listener.php

--- src/addons/Speil/BirthdayGreeter/Listener.php
+++ src/addons/Speil/BirthdayGreeter/Listener.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace DeinName\BirthdayGreeter;
+namespace Speil\BirthdayGreeter;
 
 class Listener
 {
     public static function appSetup(\XF\App $app)
     {
-        $app->cron->addJob('birthdayPoster', 'DeinName\BirthdayGreeter\Cron\BirthdayPoster::run', [
+        $app->cron->addJob('birthdayPoster', 'Speil\BirthdayGreeter\Cron\BirthdayPoster::run', [
             'runRules' => [
                 'hour' => 0,
                 'minute' => 0
